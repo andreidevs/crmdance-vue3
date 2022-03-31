1f1324e014477fe1476174927d6b39d4e0e215fb
andrei_dev <andrei.n15@mail.ru>
2022-04-01 01:28:03 +0600

    clear primevue

--- src/main.js
+++ src/main.js
@@ -4,51 +4,20 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 import "./assets/scss/index.css";
-import PrimeVue from "primevue/config";
-import Dialog from "primevue/dialog";
-import Button from "primevue/button";
-import InputText from "primevue/inputtext";
-import Password from "primevue/password";
-import Divider from "primevue/divider";
-import Dropdown from "primevue/dropdown";
-import Calendar from "primevue/calendar";
-import Card from "primevue/card";
-import Toast from "primevue/toast";
-import Menubar from "primevue/menubar";
-import Megamenu from "primevue/megamenu";
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+
+
 
 const app = createApp(App);
+
 
 app.use(router);
 app.use(createPinia());
 
-app.use(PrimeVue, { ripple: true });
+app.use(Buefy)
 // PRIME VUE INIT COMPONENTS
-import "primevue/resources/themes/lara-light-teal/theme.css"; //theme
-import "primevue/resources/primevue.min.css"; //core css
-import "primeicons/primeicons.css";
-
-app.component("Dialog", Dialog);
-
-app.component("Button", Button);
-
-app.component("InputText", InputText);
-
-app.component("Password", Password);
-
-app.component("Divider", Divider);
-
-app.component("Calendar", Calendar);
-
-app.component("Dropdown", Dropdown);
-
-app.component("Card", Card);
-
-app.component("Toast", Toast);
-
-app.component("Menubar", Menubar);
-
-app.component("MegaMenu", Megamenu);
 
 
 
